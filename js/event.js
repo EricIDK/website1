70d860b2f48176ae3468d3c24194ebeacabe4d2e
EricIDK <ericwhatthewhat6@gmail.com>
2025-03-04 17:57:54 +0000

    Tue, Mar 4, 2025, 9:57 AM -08:00

---
+++ js/event.js
@@ -0,0 +1,39 @@
+const element1 = document.getElementById('element1')
+const element2 = document.getElementById('element2')
+const element3 = document.getElementById('element3')
+
+element1.addEventListener('click', ()=>{
+    element1.innerHTML = "I've been clicked! <br> Try double clicking me."
+})
+
+element1.addEventListener('dbclick', ()=>{
+    element1.innerHTML = "I've been double clicked! <br> Try single clicking me."
+})
+
+
+element2.addEventListener('mouseover', ()=>{
+    element2.style.color = "green"
+    element2.style.backgroundColor = "blue"
+})
+
+element2.addEventListener('mouseover', ()=>{
+    element2.style.color ="white"
+    element2.style.backgroundColor = "#BB33BB"
+})
+
+let positionY = 0;
+let positionX = 0;
+
+document.addEventListener('keyup', (event)=>{
+    console.log("Hello")
+
+    if (event.key == 'ArrowDown') {
+        positionY = positionY + 10
+    }
+    else if (event.key == 'ArrowUp')
+{
+    positionY = positionY - 10
+}
+
+element3.style.top = '${positionY}px'
+})
