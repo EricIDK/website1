86901efe74ed63c3468a422ead9ff7c2d589eb5f
EricIDK <ericwhatthewhat6@gmail.com>
2025-03-05 08:54:13 -0800

    uehguh

--- js/expanding.js
+++ js/expanding.js
@@ -1,5 +1,17 @@
-const panels = document.querySeletorAll('.panel')
+const panels = document.querySelectorAll('.panel')
 
-panels.forEach(
-    
-)
+//console.log("hello")
+//console.log(panels) un comment if you want to see what they do
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () =>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
