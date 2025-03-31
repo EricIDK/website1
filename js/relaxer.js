e35cb1d43e4804b6cab64399eb1509e3c9c4402c
EricIDK <ericwhatthewhat6@gmail.com>
2025-03-31 17:12:06 +0000

    Mon, Mar 31, 2025, 10:12 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,20 +1,20 @@
-const container = document.getElementById('container')
-const text = document.getElementById('text')
+const container = document.getElementById('container');
+const text = document.getElementById('text');
 
-setInterval(breathAnimation, 7500)
+setInterval(breatheAnimation, 7500);
 
-function breathAnimation() {
-    text.innerHTML = 'Breath In'
-    container.classList.remove('shrink')
-    container.classList.add('grow')
+function breatheAnimation() {
+    text.innerHTML = 'Breathe In';
+    container.classList.remove('shrink');
+    container.classList.add('grow');
 
-    setTimeout( () => {
-        text.innerHTML = 'Hold'
-    }, 3000)
+    setTimeout(() => {
+        text.innerHTML = 'Hold';
+    }, 3000);
 
-    setTimeout( () => {
-        text.innerHTML = 'Breath Out'
-        container.classList.remove('grow')
-        container.classList.add('shrink')
-    }, 4500)
+    setTimeout(() => {
+        text.innerHTML = 'Breathe Out!';
+        container.classList.remove('grow');
+        container.classList.add('shrink');
+    }, 4500);
 }
