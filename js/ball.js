e0afab77a352ac61361a173c7acbaeb22ce03b7f
EricIDK <ericwhatthewhat6@gmail.com>
2025-04-04 17:08:34 +0000

    Fri, Apr 4, 2025, 10:08 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -2,23 +2,32 @@ const ball = document.createElement('div')
 const ballRadius = 30
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
-let ballXPosition = windowWidth/2 - ballRadius
+let ballXPosition = windowWidth / 2 - ballRadius
+let ballSpeed = 5
+let ballXDirection = 1
 
 
 setInterval(moveBall, 10)
-function moveBall(){
-    ball.style.left = ballXPosition + 5
-    ball.style.left = `${ballXPosition}px`
-}
 
 createBall()
+
+
+function moveBall() {
+    ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ball.style.left = `${ballXPosition}px`
+    if (ballXPosition > windowWidth) {
+        ballXDirection = ballXDirection * -1
+    }
+
+}
+
 function createBall(){
     document.body.appendChild(ball)
-    ball.style.height =`${2 * ballRadius}px`
-    ball.style.width =`${2 * ballRadius}px`
+    ball.style.height = `${2 * ballRadius}px`
+    ball.style.width = `${2 * ballRadius}px`
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "green"
     ball.style.position = "absolute"
-    ball.style.top = `${windowHeight/2 - ballRadius}px`
-    ball.style.left = `${windowWidth/2 - ballRadius}px`
+    ball.style.top = `${windowHeight / 2 - ballRadius}px`
+    ball.style.left = `${ballXposition}px`
 }
