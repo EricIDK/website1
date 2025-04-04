c52e9dc01332f0ea54024d3059110b88cccdb03c
EricIDK <ericwhatthewhat6@gmail.com>
2025-04-04 16:58:54 +0000

    Fri, Apr 4, 2025, 9:58 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -11,8 +11,6 @@ function createBall(){
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "green"
     ball.style.position = "absolute"
-    ball.style.top = "30px"
-    ball.style.left = "30px"
-    ball.style.top = `${windowHeight/2}px`
-    ball.style.left = `${windowHeight/2}px`
+    ball.style.top = `${windowHeight/2 - ballRadius}px`
+    ball.style.left = `${windowHeight/2 - ballRadius}px`
 }
