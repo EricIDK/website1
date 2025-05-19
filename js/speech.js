311e23db3aeec81b76df11b2e03ce5dad1cb0ccc
EricIDK <ericwhatthewhat6@gmail.com>
2025-05-19 17:29:13 +0000

    Mon, May 19, 2025, 10:29 AM -07:00

--- js/speech.js
+++ js/speech.js
@@ -7,51 +7,51 @@ const closeBtn = document.getElementById('close')
 
 const data = [
     {
-        image: '../drink.jpg',
+        image: 'drink.jpg',
         text: "I'm Thirsty"
     },
     {
-        image: '../food.jpg',
+        image: 'food.jpg',
         text: "I'm Hungry"
     },
     {
-        image: '../tired.jpg',
+        image: 'tired.jpg',
         text: "I'm Tired"
     },
     {
-        image: '../hurt.jpg',
+        image: 'hurt.jpg',
         text: "I'm Hurt"
     },
     {
-        image: '../happy.jpg',
+        image: 'happy.jpg',
         text: "I'm Happy"
     },
     {
-        image: '../angry.jpg',
+        image: 'angry.jpg',
         text: "I'm Angry"
     },
     {
-        image: '../sad.jpg',
+        image: 'sad.jpg',
         text: "I'm Sad"
     },
     {
-        image: '../scared.jpg',
+        image: 'scared.jpg',
         text: "I'm Scared"
     },
     {
-        image: '../outside.jpg',
+        image: 'outside.jpg',
         text: "I want to go outside..."
     },
     {
-        image: '../home.jpg',
+        image: 'home.jpg',
         text: "I want to go home..."
     },
     {
-        image: '../school.jpg',
+        image: 'school.jpg',
         text: "I want to go to school..."
     },
     {
-        image: '../grandma.jpg',
+        image: 'grandma.jpg',
         text: "I want to go to grandma's..."
     }
 ]
@@ -66,5 +66,42 @@ function createBox(item) {
         <img src="${item.image}" alt="${item.text}" />
         <p class="info"> ${item.text} </p>
     `
+
+    box.addEventListener('click', () => {
+        setTextMessage(text)
+        speakText()
+    })
+
     main.appendChild(box)
 }
+
+
+//toggle text box
+toggleBtn.addEventListener('click', () => {
+    document.getElementById('text-box').classList.toggle('show')
+})
+
+//close box
+closeBtn.addEventListener('click', () => {
+    document.getElementById('text-box').classList.remove('show')
+})
+
+let voices = []
+
+function getVoices() {
+    voices = speechSynthesis.getVoices()
+
+    voices.forEach(voice => {
+        const option = document.createElement('option')
+
+        option.value = voice.name
+        option.innerText = `${voice.name} ${voice.lang}`
+
+        voicesSelect.appendChild(option)
+    })
+}
+
+// voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices)
+
+getVoices()
