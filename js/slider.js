22543ad80ecabf21822323b2ed8dc56b23e18322
EricIDK <ericwhatthewhat6@gmail.com>
2025-05-28 15:59:52 +0000

    Wed, May 28, 2025, 8:59 AM -07:00

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,29 @@
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-const rightSide = document.querySelector('.right-slide');
-const leftSide = document.querySelector('.left-slide');
-const slideslength = rightSide.querySelectorAll('div').length;
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const rightSide = document.querySelector('.right-side')
+const leftSide = document.querySelector('.left-side')
+const slidesLength = rightSide.querySelectorAll('div').length
 
+// First slide
+let currentSlide = 1 //slideLength = 4
+leftSide.style.top = `${-(slidesLength - currentSlide)*100}%`
 
-upButton.addEventListener('click', () => { changeSlide('up'); } )
-downButton.addEventListener('click', () => { changeSlide('down'); } )
+upButton.addEventListener('click', () =>changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
 
 function changeSlide(direction) {
-    const topSlide = leftSide.querySelector('.top');
-    const bottomSlide = leftSide.querySelector('.bottom');
-    const rightSlide = rightSide.querySelector('.right');
-
-    if (direction === 'up') {
-        topSlide.classList.remove('top');
-        topSlide.classList.add('bottom');
-        bottomSlide.classList.remove('bottom');
-        bottomSlide.classList.add('top');
-        rightSide.appendChild(rightSlide);
-    } else if (direction === 'down') {
-        topSlide.classList.remove('top');
-        topSlide.classList.add('bottom');
-        bottomSlide.classList.remove('bottom');
-        bottomSlide.classList.add('top');
-        rightSide.prepend(rightSlide);
+    if (direction == 'up') {
+        if (currentSlide == slideLength) {
+            currentSlide = 0
+        }
+        currentSlide = currentSlide + 1
     }
+    else {
+        currentSlide = currentSlide - 1
+        if (currentSlide == 0) {
+            currentSlide = slideLength
+        }
+    }
+    leftSide.style.top = `${-(slidesLength - currentSlide)*100}%`
+    rightSide.style.top = `${-(currentSlide - 1) * 100}%`
 }
